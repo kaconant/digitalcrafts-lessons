d135403c7a7b8cd9c3857e22ec0dde675d0d743a
Krissy Conant <krissy@memberclicks.com>
2018-04-17 19:15:32 -0400

    created new ajax notes

---
+++ files/Week7/ajaxnotes.js
@@ -0,0 +1,47 @@
+/* "Our website will make a request to the server.""
+When computers talk to each other, they can only talk one at a time, back and forth. 
+
+Ajax uses HTML for this.
+HTTP: Language computers talk to each other.
+
+*---
+First step: Request --> Verb + URL
+
+Verb options: get (get resource - css file, JSON data), post (create new record of this information), 
+put (update or edit current database with this new information), del or delete (remove this row of information),
+patch (making partial changes to existing information)
+
+URL: web.com/api/dog or web.com/api/cat
+
+Request Example: get web.com/api/dog
+
+*---
+Second step: Response --> Status + Payload 
+
+Status:
+200 range - Success
+300 range - Redirection 
+400 range - Client error (ex: 404) - your fault
+500 range - Server error - their fault
+
+Payload is typically in form of JSON object:
+{ 
+    id: info,
+    name: info,
+};
+
+You can also use JSON API or GraphQL
+https://www.getpostman.com/
+
+How do you use it?
+var URL = "http://dc-coffeerun.api/coffeeorders";
+$.get(URL);
+$.get(URL, function(data) {
+    console.log(data);
+});
+$.post(URL, myData);
+$.post(URL, myData, function (resp) {
+    console.log(resp);
+})
+
+*/
