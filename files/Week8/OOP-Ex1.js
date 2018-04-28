dd2d6e629db304c73d63dd9282594e9daa56c88f
Krissy Conant <krissy@memberclicks.com>
2018-04-28 10:03:42 -0400

    updated

--- files/Week8/OOP-Ex1.js
+++ files/Week8/OOP-Ex1.js
@@ -1,3 +1,13 @@
+/*
+Write code to
+Instantiate an instance object of Person with name of 'Sonny', email of 'sonny@hotmail.com', and phone of '483-485-4948', store it in the variable sonny.
+Instantiate another person with the name of 'Jordan', email of 'jordan@aol.com', and phone of '495-586-3456', store it in the variable 'jordan'.
+Have sonny greet jordan using the greet method.
+Have jordan greet sonny using the greet method.
+Write a print statement to print the contact info (email and phone) of Sonny.
+Write another print statement to print the contact info of Jordan.
+*/
+
 function Person(name, email, phone) {
     this.name = name;
     this.email = email;
@@ -17,12 +27,3 @@ sonny.greet(jordan);
 console.log('Sonny/s Email: ' + sonny.email + 'and Sonny/s Phone: ' + sonny.phone);
 console.log('Jordan/s Email: ' + jordan.email + 'and Jordan/s Phone: ' + jordan.phone);
 
-/*
-Write code to
-Instantiate an instance object of Person with name of 'Sonny', email of 'sonny@hotmail.com', and phone of '483-485-4948', store it in the variable sonny.
-Instantiate another person with the name of 'Jordan', email of 'jordan@aol.com', and phone of '495-586-3456', store it in the variable 'jordan'.
-Have sonny greet jordan using the greet method.
-Have jordan greet sonny using the greet method.
-Write a print statement to print the contact info (email and phone) of Sonny.
-Write another print statement to print the contact info of Jordan.
-*/
